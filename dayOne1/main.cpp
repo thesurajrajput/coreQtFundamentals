e58a1a07208485c975a5810114dc8dd221ffe49c
Dobhal <suraj.dobhal@capgemini.com>
2020-07-11 05:36:01 +0530

    first commit

---
+++ dayOne1/main.cpp
@@ -0,0 +1,17 @@
+/*
+ *Author       : Suraj Dobhal
+ *Date         : 11 July 2020
+ *Developer    : Suraj
+ *Description  : dayOne of 100 Days of Code Challenge
+ */
+
+#include <QCoreApplication>
+#include <QDebug>
+int main(int argc, char *argv[])
+{
+   QCoreApplication a(argc, argv);
+
+   qInfo()<<"Hello World !!"; //prints "Hello World !!" on Console or IDE whihcever configured.
+
+   return a.exec();
+}
